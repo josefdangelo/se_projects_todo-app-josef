a6a193d810bc5bf7bc40b54326955c47d4225008
Josef D'Angelo <josefdangelo407@gmail.com>
2026-01-27 13:49:43 -0500

    tuesday january 27

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -7,8 +7,42 @@ class FormValidator {
     this._inputErrorClass = settings._inputErrorClass;
     this._inactiveButtonClass = settings._inactiveButtonClass;
     this._formElement = formElement;
+    
   }
-  enableValidation() {}
+
+  // implent other methods
+
+  _checkInputValidity(inputElement) {
+    // TODO - IMPLEMENT THIS METHOD
+    // copy (a) body of existing function
+    // work through errors in console as we did in the video
+  }
+
+  _setEventListeners() {
+    this._inputList = Array.from(
+      this._formElement.querySelectorAll(this._inputSelector),
+    );
+
+    // TODO - FINISH SETTING UP LISTENERS
+    // const buttonElement = formElement.querySelector(
+    //   settings.submitButtonSelector,
+    // );
+
+    // toggleButtonState(inputList, buttonElement, settings);
+
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        // toggleButtonState(inputList, buttonElement, settings);
+      });
+    });
+  }
+  enableValidation() {
+    this._formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
 }
 
 export default FormValidator;
